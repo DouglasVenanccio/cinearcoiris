fb4cd3b80261faabd128b59ea04aaaf0c309ef27
Douglas Venanccio <douglas.venanccio@gmail.com>
2022-12-05 07:37:19 -0300

    alterações realizadas e adições feitas

--- api/verifyToken.js
+++ api/verifyToken.js
@@ -1,20 +1,18 @@
-const fwt = require("jsonwebtoken");
-const User = require("./models/User");
+const jwt = require("jsonwebtoken");
 
-function verify(req, res, next){
-    const authHeader =  req.headers.token;  
-    if(authHeader){
-        const token = authHeader.split("")[1];
+function verify(req, res, next) {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
 
-        fwt.verify(token, process.env.SECRET_KEY,(err,user)=>{
-            if(err) res.status(403).json("O Token não é Valido!");
-            req.user = user;
-            next();
-        });
-
-    }else{
-        return res.status(401).json("Você não está Autenticado!");
-    }
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+      if (err) res.status(403).json("O Token não é Valido!");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("Você não está Autenticado!");
+  }
 }
 
 module.exports = verify;
